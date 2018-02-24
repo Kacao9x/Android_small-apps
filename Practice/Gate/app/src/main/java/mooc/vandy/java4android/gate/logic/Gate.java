bae4cd1e58e581ce120114aa529a013020ec161b
Kacao Cao <kacao@iastate.edu>
2018-02-23 19:16:44 -0500

    add 2 code skeleton for practice problems

---
+++ Practice/Gate/app/src/main/java/mooc/vandy/java4android/gate/logic/Gate.java
@@ -0,0 +1,29 @@
+package mooc.vandy.java4android.gate.logic;
+
+/**
+ * This file defines the Gate class.
+ */
+public class Gate {
+    public static final Object CLOSED = ;
+    public static final Object IN = ;
+    public static final Object OUT = ;
+
+    public Gate () {
+
+    }
+
+    public String getSwingDirection() {
+    }
+
+    public void setSwing(Object in) {
+    }
+
+    public void close() {
+    }
+
+    public void open(Object in) {
+    }
+    // TODO -- Fill in your code here
+
+
+}
