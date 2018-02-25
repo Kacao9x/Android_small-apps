569d38ea82a4a6ba4fe7a62daa3bb76b7512378c
kacao cao <kacao@iastate.edu>
2018-02-25 16:06:23 -0500

    Finished creating Gate class. Failed to pass the unit test

--- Practice/Gate/app/src/main/java/mooc/vandy/java4android/gate/logic/Gate.java
+++ Practice/Gate/app/src/main/java/mooc/vandy/java4android/gate/logic/Gate.java
@@ -4,26 +4,68 @@ package mooc.vandy.java4android.gate.logic;
  * This file defines the Gate class.
  */
 public class Gate {
-    public static final Object CLOSED = ;
-    public static final Object IN = ;
-    public static final Object OUT = ;
+    public static final int CLOSED = 0;
+    public static final int IN = 1;
+    public static final int OUT = -1 ;
+    private int mSwing = 0;
+    private int total = 0;
 
     public Gate () {
+        this.mSwing = CLOSED;
+    }
+    public Gate (int swing) {
+        this.mSwing = swing;
+    }
+    // TODO -- Fill in your code here
 
+    public int getSwingDirection() {
+        return this.mSwing;
     }
 
-    public String getSwingDirection() {
-    }
-
-    public void setSwing(Object in) {
+    /**
+     * setter for mSwing direction
+     * @param dir: value of direction
+     * @return false if no change in value, otherwise true if successfully set
+     */
+    public boolean setSwing(int dir) {
+        if( this.mSwing != dir ) {
+            this.mSwing = dir;
+            return true;
+        }
+        return false;
     }
 
     public void close() {
+        setSwing(CLOSED);
+    }
+    public boolean open(int dir) {
+        if( dir == IN || dir == OUT ) {
+            setSwing(dir);
+            return true;
+        }
+        return false;
     }
 
-    public void open(Object in) {
+    public int thru(int count) {
+        if( getSwingDirection() > 0 ){
+            return count;
+        } else if( getSwingDirection() < 0 ) {
+            return -count;
+        } else
+            return 0;
     }
-    // TODO -- Fill in your code here
+
+
+    public String toString() {
+        if( this.getSwingDirection() == IN )
+            return "This gate is open and swings to enter the pen only";
+        else if ( this.getSwingDirection() == OUT )
+            return "This gate is open and swings to exit the pen only";
+        else if ( this.getSwingDirection() == CLOSED)
+            return "The gate is CLOSED";
+        else
+            return "This gate has an invalid swing direction";
+    }
 
 
 }
