e0f79800d2cecb3d7beae6daeaa52264162ac5af
Kacao Cao <kacao@iastate.edu>
2018-03-12 19:31:42 -0400

    Testing Fill_the_corral: all method passed except corralSnails. Halted at randomly select number to snails to move

--- Practice/Gate-Client/app/src/main/java/mooc/vandy/java4android/gate/logic/FillTheCorral.java
+++ Practice/Gate-Client/app/src/main/java/mooc/vandy/java4android/gate/logic/FillTheCorral.java
@@ -31,14 +31,24 @@ public class FillTheCorral {
      */
     public void corralSnails(Gate[] corral, Random rand) {
         int snailsInPasture = 5;
-        int snailsToMove = rand.nextInt(snailsInPasture);
-        while (snailsInPasture >0 ) {
-            Gate G = corral[rand.nextInt(corral.length)];       //select a random Gate
+        int count = 0;
+
+        while (snailsInPasture != 0 ) {
+            int num = rand.nextInt(corral.length);
+            Gate G = corral[num];       //select a random Gate
+            int snailsToMove = rand.nextInt(snailsInPasture);
 
             if (G.getSwingDirection() != 0) {
-                snailsInPasture = updateSnailInPasture(G, snailsInPasture, snailsToMove);
+                snailsInPasture += G.thru(snailsToMove);
+//                snailsInPasture += updateSnailInPasture(G,
+//                                        snailsInPasture, snailsToMove);
+                mOut.println(snailsToMove + " are trying to move through corral "
+                            + num);
+                count++;
             }
+
         }
+
 
     }
 
@@ -68,7 +78,17 @@ public class FillTheCorral {
     public void setCorralGates(Gate[] corral, Random random) {
         int[] gateSwing = {-1, 0, 1};
         for (int i=0; i < corral.length; i++) {
-            corral[i].open(random.nextInt(gateSwing.length));
+            corral[i].open(random.nextInt(gateSwing.length)+1);
+
+            if(corral[i].getSwingDirection() == 0)
+                mOut.println("Gate " + i +": This gate is CLOSED");
+            else {
+                mOut.print("Gate " + i +": This gate is open and swings to ");
+                if(corral[i].getSwingDirection() == 1)
+                    mOut.println("ENTER");
+                else if(corral[i].getSwingDirection() == -1)
+                    mOut.println("EXIT");
+            }
         }
     }
 
